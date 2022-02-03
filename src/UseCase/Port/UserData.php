30f54e1b19c6053b719dc2468c45888097038a2b
Augusto Cesar Ferreira <gutocf@gmail.com>
2022-02-02 23:50:26 -0300

    Uses constructor property promotion

--- src/UseCase/Port/UserData.php
+++ src/UseCase/Port/UserData.php
@@ -4,16 +4,11 @@ namespace App\UseCase\Port;
 
 class UserData
 {
-    public int $id;
-    public string $name;
-    public string $email;
-    public string $password;
-
-    public function __construct(int $id, string $name, string $email, string $password)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->email = $email;
-        $this->password = $password;
+    public function __construct(
+        public int $id,
+        public string $name,
+        public string $email,
+        public string $password
+    ) {
     }
 }
